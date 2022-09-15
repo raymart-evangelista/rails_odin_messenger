4a683e7af396a4eb9172dc958d93309e5e0be61c
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-15 16:26:50 -0700

    add routes, association, and #index msg obj

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'hangouts#index'
   devise_for :users
+
+  resources :messages, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
